ef825836018dd45273aa2d560c237101328386b2
Dipen Patel <dipen.ptl1@gmail.com>
2024-05-09 11:58:46 +0300

    Unit test configuration

--- __test__/App.test.js
+++ __test__/App.test.js
@@ -1,3 +1,19 @@
-it("works", () => {
-  expect(1).toBe(1);
+import React from "react";
+import renderer from "react-test-renderer";
+import App from "../App";
+
+describe("<Basic />", () => {
+  it("works", () => {
+    expect(1).toBe(1);
+  });
+});
+
+describe("<App />", () => {
+  it("works", () => {
+    expect(1).toBe(1);
+  });
+  it("has 1 child", () => {
+    const tree = renderer.create(<App />).toJSON();
+    expect(tree.children.length).toBe(1);
+  });
 });
